a1b34e37295ee869aaf2c9fb835a7dd63d56829f
Ivan Malison <IvanMalison@gmail.com>
2026-07-20 10:33:01 -0700

    feat(macro): expose custom host and runnable hooks

--- rmk-macro/src/codegen/override_helper.rs
+++ rmk-macro/src/codegen/override_helper.rs
@@ -1,10 +1,11 @@
 use darling::FromMeta;
 
 /// List of functions that can be overwritten
-#[derive(Debug, Clone, Copy, FromMeta)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, FromMeta)]
 pub enum Overwritten {
     Usb,
     ChipConfig,
     ChipInit,
+    HostService,
     Entry,
 }
