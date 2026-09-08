0a58ffcea114c1a4ae5ca14fede66c2f509419bc
Ivan Malison <IvanMalison@gmail.com>
2026-09-08 01:03:36 -0700

    fork-assembler: fixup fold/runtime-behavior-parity

--- rmk/src/host/context.rs
+++ rmk/src/host/context.rs
@@ -351,6 +351,8 @@ impl<'a> KeyboardContext<'a> {
             return false;
         }
         #[cfg(feature = "storage")]
+        let hold_trigger_positions = self.keymap.morse_hold_trigger_positions();
+        #[cfg(feature = "storage")]
         {
             FLASH_CHANNEL
                 .send(FlashOperationMessage::MorseProfile {
@@ -363,6 +365,9 @@ impl<'a> KeyboardContext<'a> {
                     idx,
                     name: MorseProfileName::new(),
                 })
+                .await;
+            FLASH_CHANNEL
+                .send(FlashOperationMessage::MorseHoldTriggerPositions(hold_trigger_positions))
                 .await;
         }
         true
